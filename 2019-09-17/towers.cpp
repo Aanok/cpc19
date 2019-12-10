2b4f1fe12f7150d23bfaeb1bdf79c47cf864f463
Fabrizio Baldini <18417628+Aanok@users.noreply.github.com>
2019-12-10 11:10:35 +0100

    maybe add analysis too, eh?

--- 2019-09-17/towers.cpp
+++ 2019-09-17/towers.cpp
@@ -1,3 +1,21 @@
+// STRATEGY
+// The problem consists of finding how many unique elements exist in the input
+// sequence and how many there are of the most frequent one.
+//
+// Since both the input domain and size of input sequence are known beforehand,
+// we can solve this by simply allocating an array of counters as long as the
+// cardinality of the input domain. The counters start from zero, and we
+// increase counter I whenever we witness the number I in the input sequence.
+//
+// Two bookkeeping variables let us track the number of unique sightings and
+// the number of sightings of the most frequent input number.
+//
+// COST
+// For K cardinality of the input domain, we require O(K) memory overhead for
+// the array of counters and O(1) for bookkepping variables.
+// The time complexity is O(N) as we just need to scan the input sequence.
+
+
 #include <vector>
 #include <iostream>
 #include <cassert>
