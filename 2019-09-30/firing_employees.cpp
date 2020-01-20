182b2637a8a85bb1a84edd92436823a13e611c0e
Fabrizio Baldini <18417628+Aanok@users.noreply.github.com>
2020-01-20 11:25:11 +0100

    firing employees wip

--- 2019-09-30/firing_employees.cpp
+++ 2019-09-30/firing_employees.cpp
@@ -1,28 +1,67 @@
-#include <iostream>
-#include <cmath>
+#include <unordered_set>
 #include <vector>
+#include <cmath>
+#include <iostream>
 
-// TODO: precompute all primes once per company instead of once per employee
 
-bool is_prime(const size_t n)
+////////// CACHING SIEVE //////////
+class caching_sieve
+{
+    public:
+        caching_sieve();
+        bool is_prime(const size_t n);
+
+    private:
+        size_t m_size;
+        std::unordered_set<size_t> m_primes;
+
+        void update_sieve(const size_t n);
+};
+
+
+caching_sieve::caching_sieve()
+    : m_size(0), m_primes()
+{
+    update_sieve(100);
+}
+
+
+void caching_sieve::update_sieve(const size_t n)
 {
     std::vector<bool> candidates(n + 1, true);
     size_t p = 2;
     candidates[0] = false;
     candidates[1] = false;
 
-    while (true) {
-        if (p > std::sqrt(n)) return true;
+    while (p <= std::sqrt(n)) {
         size_t f = p;
         while (p * f <= n) {
-            if (p * f == n) return false;
             candidates[p * f] = false;
             f++;
         }
         p++;
         while (candidates[p] == false) p++;
     }
+
+    for (size_t i = 0; i < n; i++) {
+        if (candidates[i] == true) {
+            m_primes.insert(i);
+        }
+    }
+
+    m_size = n;
 }
+
+
+bool caching_sieve::is_prime(const size_t n)
+{
+    if (n > m_size) {
+        update_sieve(n);
+    }
+
+    return m_primes.find(n) != m_primes.end();
+}
+///////////////////////////////////
 
 
 size_t count_seniors(const std::vector<size_t> &company, size_t i)
@@ -38,14 +77,14 @@ size_t count_seniors(const std::vector<size_t> &company, size_t i)
 }
 
 
-size_t count_fireable(const std::vector<size_t> &company)
+size_t count_fireable(const std::vector<size_t> &company, caching_sieve &primes)
 {
     size_t count = 0;
 
     for (size_t i = 0; i < company.size(); i++) {
         size_t seniors = count_seniors(company, i);
         if (seniors == 0) continue;
-        if (is_prime(i + 1 + seniors)) count++;
+        if (primes.is_prime(i + 1 + seniors)) count++;
     }
 
     return count;
@@ -56,6 +95,7 @@ int main(int argc, char *argv[])
 {
     size_t t;
     std::vector<size_t> company;
+    caching_sieve primes;
 
     std::cin >> t;
     for (size_t i = 0; i < t; i++) {
@@ -68,7 +108,7 @@ int main(int argc, char *argv[])
             std::cin >> tmp;
             company.push_back(tmp);
         }
-        std::cout << count_fireable(company) << std::endl;
+        std::cout << count_fireable(company, primes) << std::endl;
     }
 
     return EXIT_SUCCESS;
