e533d12575280641506b2f784f4faac3aec73f84
Aanok <18417628+Aanok@users.noreply.github.com>
2020-01-23 11:02:03 +0100

    firing employees

--- 2019-09-30/firing_employees.cpp
+++ 2019-09-30/firing_employees.cpp
@@ -1,6 +1,49 @@
-#include <unordered_set>
+// STRATEGY
+// There are two parts to this problem.
+//
+// The first is to determine if a given number is prime or not. As by
+// http://research.cs.wisc.edu/techreports/1990/TR909.pdf we see the classic
+// Sieve of Erathosthenes is actually a solid choice of algorithm for small
+// inputs, like the ones from the problem. As the problem then requires
+// answering many such queries within a given range per instance, we add the
+// optimization of precomputing all primes within the range once before starting
+// answering queries (we actually keep such a cache persistent between the
+// various instances too: no reason to throw away work that may be useful again
+// in the future).
+//
+// The second part regards computing the depth of a node within a tree
+// represented by an array of parent node ID's. This is done easily and
+// efficiently once you make sure to save the depths of all ancestors of a 
+// queried node and not just that of the queried node. A recursive form of the
+// algorithm would look something like this:
+//
+// COMPUTE_DEPTH(parents, node)
+//  if (parents[node] == null) then
+//      node.depth = 0
+//  else
+//      if (parents[node].depth == null) then
+//          COMPUTE_DEPTH(parents, parents[node])
+//      end
+//      node.depth = parents[node].depth + 1
+//  end
+// end
+//
+// As the trees can hold up to 10^5 nodes, we apply the optimization of
+// unrolling the recursive structure into an iterative form by keeping an
+// explicit backtracking stack. This is quicker and, most importantly, avoids
+// call stack overflows.
+//
+// COST
+// Let N be the size of the company (number of employees).
+// The first part takes O(N) time and O(N) space, as we employ a simple
+// implementation of the Sieve of Erathostenes using a bitvector.
+// The second part likewise takes O(N) time (each node is pushed and popped on
+// the stack once) and O(N) space (we save the nodes' depth in an array).
+
+
 #include <vector>
 #include <cmath>
+#include <stack>
 #include <iostream>
 
 
@@ -8,45 +51,41 @@
 class caching_sieve
 {
     public:
-        caching_sieve();
+        caching_sieve(const size_t n = 100);
         bool is_prime(const size_t n);
+        void update_sieve(const size_t n);
 
     private:
         size_t m_size;
-        std::unordered_set<size_t> m_primes;
+        std::vector<bool> m_primes;
 
-        void update_sieve(const size_t n);
 };
 
 
-caching_sieve::caching_sieve()
+caching_sieve::caching_sieve(const size_t n)
     : m_size(0), m_primes()
 {
-    update_sieve(100);
+    update_sieve(n);
 }
 
 
 void caching_sieve::update_sieve(const size_t n)
 {
-    std::vector<bool> candidates(n + 1, true);
+    // a smarter implementation would not throw away the old vector and extend
+    // it using a segmented sieve algorithm but this is not necessary for the
+    // exercise
+    m_primes = std::vector<bool> (n + 1, true);
     size_t p = 2;
-    candidates[0] = false;
-    candidates[1] = false;
+    m_primes[0] = false;
+    m_primes[1] = false;
 
     while (p <= std::sqrt(n)) {
         size_t f = p;
         while (p * f <= n) {
-            candidates[p * f] = false;
+            m_primes[p * f] = false;
             f++;
         }
-        p++;
-        while (candidates[p] == false) p++;
-    }
-
-    for (size_t i = 0; i < n; i++) {
-        if (candidates[i] == true) {
-            m_primes.insert(i);
-        }
+        while (m_primes[++p] == false) ;
     }
 
     m_size = n;
@@ -59,32 +98,49 @@ bool caching_sieve::is_prime(const size_t n)
         update_sieve(n);
     }
 
-    return m_primes.find(n) != m_primes.end();
+    return m_primes[n];
 }
 ///////////////////////////////////
 
 
-size_t count_seniors(const std::vector<size_t> &company, size_t i)
+// NB return by value is efficient as by reference due to copy elision
+// see http://web.archive.org/web/20140113221447/http://cpp-next.com/archive/2009/08/want-speed-pass-by-value/
+std::vector<size_t> 
+count_seniors(const std::vector<size_t> &company)
 {
-    size_t count = 0;
+    std::vector<size_t> seniors(company.size(), -1); // NB max size_t is a valid sentinel by input constraint
+    std::stack<size_t> backtrack;
 
-    while (company[i] != 0) {
-        count++;
-        i = company[i] - 1;
+    for (size_t i = 0; i < company.size(); i++) {
+        if (seniors[i] != -1) continue;
+        backtrack.push(i);
+        while (! backtrack.empty()) {
+            size_t j = backtrack.top();
+            if (company[j] == 0) {
+                seniors[j] = 0;
+                backtrack.pop();
+            } else if (seniors[company[j] - 1] != -1) {
+                seniors[j] = seniors[company[j] - 1] + 1;
+                backtrack.pop();
+            } else {
+                backtrack.push(company[j] - 1);
+            }
+        }
     }
 
-    return count;
+    return seniors;
 }
 
 
 size_t count_fireable(const std::vector<size_t> &company, caching_sieve &primes)
 {
     size_t count = 0;
+    std::vector<size_t> seniors = count_seniors(company);
 
+    primes.update_sieve(2 * (company.size() + 1));
     for (size_t i = 0; i < company.size(); i++) {
-        size_t seniors = count_seniors(company, i);
-        if (seniors == 0) continue;
-        if (primes.is_prime(i + 1 + seniors)) count++;
+        if (seniors[i] == 0) continue;
+        if (primes.is_prime(i + 1 + seniors[i])) count++;
     }
 
     return count;
