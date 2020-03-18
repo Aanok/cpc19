0f3fce896340e36aa8381f394c057e460137bec8
Aanok <fabrizio.baldini@protonmail.com>
2020-03-18 23:33:33 +0100

    findpair

--- 2019-10-07/findpair.cpp
+++ 2019-10-07/findpair.cpp
@@ -1,3 +1,8 @@
+// COST
+// Computation: O(N*log(N)
+// Space: O(N)
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,22 +10,32 @@
 
 int main (int argc, char *argv[])
 {
-    uint16_t n, k;
+    uint64_t n, k, multiplicity, i;
+    uint64_t smaller = 0;
     std::vector<int32_t> input;
 
     std::cin >> n >> k;
     input.reserve(n);
     k--; // make it 0-indexed
 
-    for (uint16_t i = 0; i < n; i++) {
+    for (uint64_t i = 0; i < n; i++) {
         int32_t tmp;
         std::cin >> tmp;
         input.push_back(tmp);
     }
 
     std::sort(input.begin(), input.end());
+    
+    i = k / n;
 
-    std::cout << input[k / n] << " " << input[k % n] << std::endl;
+    multiplicity = std::count(input.cbegin(), input.cend(), input[i]);
+
+    while (smaller < n && input[smaller] != input[i]) smaller++;
+
+    std::cout
+        << input[k / n]
+        << " " << input[(k - smaller * n) / multiplicity]
+        << std::endl;
 
     return EXIT_SUCCESS;
 }
